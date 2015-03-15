7bf0b2eaaf2dca6731ee67eff68ff86d829a1653
hanantahayni <hanan.tahayni20@gmail.com>
2015-03-15 17:24:15 +0200

    update

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ app.config(['$routeProvider', function($routeProvider) {
     //$routeProvider.when('/deletePost', {templateUrl: 'views/deletePostView.html', controller: 'deletePostCtrl'});
     $routeProvider.when('/newsfeed', {templateUrl: 'views/newsfeedView.html', controller: 'newsfeedCtrl'});
     $routeProvider.when('/newsfeed/:postId', {templateUrl: 'views/singlePostView.html', controller: 'singlePostCtrl'});
-
+    $routeProvider.when('/sendMessage', {templateUrl: 'views/sendMessageView.html', controller: 'sendMessageCtrl'});
 
     $routeProvider.when('/login', {templateUrl: 'views/loginView.html', controller: 'loginCtrl'});
     $routeProvider.when('/main', {templateUrl: 'views/mainView.html', controller: 'mainCtrl'});
