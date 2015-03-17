15f240399dec2c4f2b453712bf3e1d6ec1d8be82
esmatptr <pointer.ptr@gmail.com>
2015-03-17 22:33:04 +0200

    New changelist

--- dist/js/script.js
+++ dist/js/script.js
@@ -66,4 +66,22 @@
 		}
 	}); 
 	
-})();  
+})();
+
+
+
+
+
+
+$(document).ready(function () {
+    $( ".change_to_left" ).click(function() {
+        $('.wrap').removeClass('sidebar-right');
+        $('.wrap').addClass('sidebar-left');
+    });
+
+    $( ".change_to_right" ).click(function() {
+        $('.wrap').removeClass('sidebar-left');
+        $('.wrap').addClass('sidebar-right');
+    });
+
+});
