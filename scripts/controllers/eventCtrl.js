e70ce78c6c3aff56f97b8f9dc5c7723c92efae06
rana <rana.duaibes@gmail.com>
2015-04-29 09:21:09 +0300

    Event(services, controller) with modal to be changed

---
+++ scripts/controllers/eventCtrl.js
@@ -0,0 +1,152 @@
+/**
+ * Created by rana on 4/27/2015.
+ */
+
+
+
+angular.module('myApp').controller('eventCtrl',
+    function($rootScope,$scope, eventEntitiesService, eventRequestService,$routeParams, authenticationService) {
+
+        $scope.js_id= authenticationService.userProfile.jobseekerId;
+        $scope.getEvents=function(){
+            var jobId=$routeParams.jobId;
+
+
+            $scope.events=[
+
+            ];
+            var getEventsEntity = eventEntitiesService.getEvent(jobId,$scope.js_id);
+            var eventPromise =eventRequestService.getEvents(getEventsEntity);
+
+            eventPromise.then(function (d) {
+                console.log(d);
+                $scope.events= d.data;
+
+
+
+            }, function (d) {
+                swal({
+                    title: "Error!",
+                    text: "Something went wrong, please try again later",
+                    type: "error"
+                });
+            });
+
+        };
+
+
+
+        $scope.addEvent = function () {
+            var jobId=$routeParams.jobId;
+            var js_id=authenticationService.userProfile.jobseekerId;
+
+
+            var eventEntity = eventEntitiesService.addEvent(jobId,js_id,$scope.eventTitle,$scope.eventDetail, $scope.remainderDate )
+            var eventPromise = eventRequestService.addEvent(eventEntity);
+
+
+            eventPromise.then(function (d) {
+                console.log(d);
+                var event= d.data;
+                //$scope.jobId= event.jobId;
+                //$scope.js_id= event.js_id;
+                //$scope.eventTitle= event.eventTitle;
+                //$scope.eventDetail= event.eventDetail;
+                //$scope.remainderDate= event.remainderDate;
+                //
+                //
+                //
+                //$scope.jobComments.unshift({jobId:event.jobId,js_id:event.js_id,eventTitle:event.eventTitle,eventDetail:event.eventDetail,reminderDate:event.remainderDate,eventId:event.eventId});
+                //
+                //$scope.eventTitle="";
+                //$scope.eventDetail="";
+
+            }, function (d) {
+                swal({
+                    title: "Error!",
+                    text: "Something went wrong, please try again later",
+                    type: "error",
+                    timer: 2000
+                });
+            });
+        };
+
+
+
+        //$scope.showModal = false;
+        //$scope.toggleModal = function(comment_id,content){
+        //    $scope.showModal = !$scope.showModal;
+        //    $scope.editedContent=content;
+        //    $scope.editedCommentId=comment_id;
+        //};
+
+        $scope.editComment=function(editedEventId,editedEventDetail, editedEventTitle,editedRemainderDate){
+            for (var i=0; i<$scope.events.length; i++) {
+                if ($scope.events[i].eventId == editedEventId) {
+                    $scope.events[i].eventDetail = editedEventDetail;
+                    $scope.events[i].eventTitle = editedEventTitle;
+                    $scope.events[i].remainderDate = editedRemainderDate;
+
+
+                    break;
+                }
+            }
+            $scope.showModal = false;
+
+            var editEventEntity = eventEntitiesService.editEvent(editedEventId,editedEventDetail,editedEventTitle,editedReminderDate);
+            var eventPromise = eventRequestService.editEvent(editEventEntity);
+
+            eventPromise.then(function (d) {
+                console.log(d);
+
+
+
+
+            }, function (d) {
+                swal({
+                    title: "Error!",
+                    text: "Something went wrong, please try again later",
+                    type: "error"
+                });
+            });
+        }
+
+        $scope.deleteEvent=function(eventId) {
+            swal({
+                    title: "Are you sure?",
+                    text: "Delete!",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Yes, delete!",
+                    closeOnConfirm: true },
+                function() {
+
+                    var deleteEventEntity = eventEntitiesService.deleteEventEntity(eventId);
+                    var eventPromise = eventRequestService.deleteEvent(deleteEventEntity);
+
+
+                    //delete event
+                    for (var i = 0; i < $scope.events.length; i++) {
+                        if ($scope.events[i].eventId == eventId) {
+                            $scope.events.splice(i, 1);
+                            break;
+                        }
+                    }
+                    eventPromise.then(function (d) {
+
+
+                    }, function (d) {
+                        swal({
+                            title: "Error!",
+                            text: "Something went wrong, please try again later",
+                            type: "error"
+                        });
+                    });
+                }
+            );
+
+        }
+
+
+    });
