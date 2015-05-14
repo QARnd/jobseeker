562e477d244efd16ea43a1b8b5bbd66db2292c22
amoosha <omayma.abuelrub@gmail.com>
2015-05-14 22:48:19 +0300

    jobHistory

--- server/Jobseeker_Form.php
+++ server/Jobseeker_Form.php
@@ -176,6 +176,9 @@ class Jobseeker_Form extends Jobseeker_DB {
                 case 'getMsgsHistoryRequest':
                     $this->getMsgsHistoryRequest();
                     break;
+                case 'getJobsHistoryRequest':
+                    $this->getJobsHistory();
+                    break;
 
 
 
@@ -1180,6 +1183,25 @@ public function sendEmailToP(){
             array_push($total, $row);
         }
 
+
+        print(json_encode($total));
+    }
+
+
+
+
+    public function getJobsHistory(){
+        +       $entity='Entity';
+                $js_id='js_id';
+                $js_id=$GLOBALS['request']->$entity->$js_id;
+
+        //        $sql='select count(*) AS "counts", joblist.jobId, notifications.content, notifications.alertDate from joblist,notifications where notifications.notiToId='.$js_id.'and notifications.notiToId=joblist.jobseekerId order by joblist.jobId DESC limit'. $countNot;
+                $sql='select jobNotification.content,jobNotification.alertDate,jobNotification.jobId from jobNotification,jobseekers  where jobNotification.notiToId='.$js_id;
+                $result=$GLOBALS['db']->db_query($sql);
+               $total=array();
+                while($row = $GLOBALS['db']->db_assoc($result)){
+                        array_push($total, $row);
+                    }
 
         print(json_encode($total));
     }
