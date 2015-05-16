f426fbdf7212eab19f27208cde80ff2c019f916f
esmatptr <pointer.ptr@gmail.com>
2015-05-16 20:17:25 +0300

    Login View

--- scripts/controllers/loginCtrl.js
+++ scripts/controllers/loginCtrl.js
@@ -67,10 +67,14 @@ angular.module('myApp')
         },
 
 
-
         $scope.showModalAccount = false;
         $scope.toggleAccount = function(){
             $scope.showModalAccount = !$scope.showModalAccount;
 
-        };
+        },
+        $scope.showPostsModal = false;
+        $scope.postsToggle = function(){
+            $scope.showPostsModal = !$scope.showPostsModal;
+
+        }
     });
